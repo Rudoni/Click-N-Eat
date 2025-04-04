fee1539af83c10ef313f55cf98e27cd056a1b12b
SIMON Thomas <thomas.simon4@etu.univ-lorraine.fr>
2025-04-04 15:38:27 +0200

    deleteRestaurant 1

--- services/restaurant/routes/serveur_restaurant.routes.js
+++ services/restaurant/routes/serveur_restaurant.routes.js
@@ -3,5 +3,6 @@ const authController = require("../controllers/serveur_restaurant.controllers");
 module.exports = function(app) {
     app.post("/addRestaurant", authController.addRestaurant);
     app.post("/addArticle", authController.addArticle);
+    app.post("/deleteRestaurant", authController.deleteRestaurant);
     app.post("/test", authController.test_api)
 };
