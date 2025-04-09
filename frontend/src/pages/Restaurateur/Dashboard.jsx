1d98aee6c2fdfc3fb0f0ef62b2d5dd3f1ab9b353
AdamMhr <mahraoui30@gmail.com>
2025-04-09 10:47:30 +0200

    dashboard

---
+++ frontend/src/pages/Restaurateur/Dashboard.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import './Dashboard.css';
+
+const RestaurantDashboard = () => {
+  const [restaurant, setRestaurant] = useState({
+    name: '',
+    description: ''
+  });
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    const fetchRestaurantInfo = async () => {
+      try {
+        const response = await fetch("http://localhost:3100/getRestaurantInfo", {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({})
+        });
+
+        const json = await response.json();
+        if (response.ok) {
+        console.log(json)
+          setRestaurant({
+            name: json.data.restaurant_name || '',
+            description: json.data.restaurant_description || ''
+          });
+        } else {
+          console.error("Erreur lors de la récupération des infos restaurant");
+        }
+      } catch (error) {
+        console.error("Erreur serveur :", error);
+      }
+    };
+
+    fetchRestaurantInfo();
+  }, []);
+
+  return (
+    <section className="restaurant-dashboard">
+      <h1 className="dashboard-title">Bienvenue sur votre tableau de bord</h1>
+      <div className="restaurant-card">
+        <h2 className="restaurant-name">{restaurant.name}</h2>
+        <p className="restaurant-description">{restaurant.description}</p>
+      </div>
+    </section>
+  );
+};
+
+export default RestaurantDashboard;
