e4625701cce815b017fa41f9432d39f526b45c03
AdamMhr <mahraoui30@gmail.com>
2025-04-10 15:18:04 +0200

    lien footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,18 +8,26 @@ const Footer = () => {
       <div className="footer-col">
         <h2>Click'N’Eat</h2>
         <div className="social-icons">
-          <a href="#"><img src="/icons/twitter.svg" alt="Twitter" /></a>
-          <a href="#"><img src="/icons/facebook.svg" alt="Facebook" /></a>
-          <a href="#"><img src="/icons/instagram.svg" alt="Instagram" /></a>
-          <a href="#"><img src="/icons/linkedin.svg" alt="LinkedIn" /></a>
-        </div>
+  <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+    <img src="/icons/twitter.svg" alt="Twitter" />
+  </a>
+  <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+    <img src="/icons/facebook.svg" alt="Facebook" />
+  </a>
+  <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+    <img src="/icons/instagram.svg" alt="Instagram" />
+  </a>
+  <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+    <img src="/icons/linkedin.svg" alt="LinkedIn" />
+  </a>
+</div>
       </div>
 
       <div className="footer-col">
-        <a href="#">Obtenir de l’aide</a>
-        <a href="#">Ajouter votre restaurant</a>
-        <a href="#">Devenir livreur</a>
-        <a href="#">Devenir développeur tiers</a>
+      <Link to="/contact">Obtenir de l’aide</Link>
+      <Link to="/register">Ajouter votre restaurant</Link>
+      <Link to="/register">Devenir livreur</Link>
+      <Link to="/register">Devenir développeur tiers</Link>
       </div>
 
       <div className="footer-col">
